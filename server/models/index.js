6c566ee48f993f056fa41207d4b2fe59d5d8039a
emasysnd <emrain02@gmail.com>
2017-11-01 20:42:24 +0100

    Chore: #152485137 Deploy to heroku

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,47 @@
+// const fs = require('fs');
+// const path = require('path');
+// const Sequelize = require('sequelize');
+// const config = require('../config/config');
+
+// const basename = path.basename(module.filename);
+
+// const db = {};
+
+// let sequelize;
+// if (process.env.DATABASE_URL) {
+//   // the application is executed on Heroku ... use the postgres database
+//   sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     logging: true // false
+//   });
+//   // sequelize = new Sequelize(process.env[config.use_env_variable]);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+// fs
+//   .readdirSync(__dirname)
+//   .filter(file =>
+//     (file.indexOf('.') !== 0) &&
+//     (file !== basename) &&
+//     (file.slice(-3) === '.js'))
+//   .forEach((file) => {
+//     const model = sequelize.import(path.join(__dirname, file));
+//     db[model.name] = model;
+//   });
+
+// Object.keys(db).forEach((modelName) => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// export default db;
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -8,16 +52,12 @@ const basename = path.basename(module.filename);
 const db = {};
 
 let sequelize;
-if (process.env.DATABASE_URL) {
-  // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    logging: true // false
-  });
-  // sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database, config.username, config.password, config
+  );
 }
 
 fs
