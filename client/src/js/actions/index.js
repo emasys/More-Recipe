11bed6b471178817e2bc0f5bf4516c5ef21a940d
emasysnd <emrain02@gmail.com>
2017-11-08 19:34:11 +0100

    Add route to allow users view profile

--- client/src/js/actions/index.js
+++ client/src/js/actions/index.js
@@ -1,13 +1,11 @@
 const URL = 'http://localhost:8081/api/v1/recipes';
 
-export function fetchRecipes(){
-    const req = fetch(URL, {method: 'GET'})
-    .then(res => res.json());
-    return {type: 'RECIPES', payload: req}
+export function fetchRecipes() {
+  const req = fetch(URL, { method: 'GET' }).then(res => res.json());
+  return { type: 'RECIPES', payload: req };
 }
 
-// export function latestNews() {
-//   const req = fetch(`${URL}/articles?_order=desc&_end=3`, {method: 'GET'})
-//   .then(res => res.json());
-//   return {type: 'LATEST_NEWS', payload: req}
-// }
+export function getRecipe(id) {
+  const req = fetch(`${URL}/id`, { method: 'GET' }).then(res => res.json());
+  return { type: 'RECIPE', payload: req };
+}
