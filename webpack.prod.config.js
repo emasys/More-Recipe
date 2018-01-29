e5e8c469af7418825c133f1e61f285943ce6f9d8
Emmanuel Ndukwe <emasysnd@gmail.com>
2018-01-29 20:26:08 +0100

    refactor webpack

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './client/src/app.jsx',
@@ -9,10 +10,8 @@ module.exports = {
     filename: 'bundle.js'
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
+    new Dotenv({
+      systemvars: true
     }),
     new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.HotModuleReplacementPlugin(),
@@ -40,7 +39,7 @@ module.exports = {
     extensions: ['.js', '.json', '.jsx', '.css', '.scss'],
     descriptionFiles: ['package.json']
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'source-map',
   module: {
     rules: [
       {
